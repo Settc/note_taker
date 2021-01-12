03676df035f6bef2ec481ea7bfbd0d8b2de7017e
Thomas Whittle <tewhittle@gmail.com>
2021-01-12 12:20:31 -0500

    Add initial structure with routes and express framework

---
+++ Develop/db/notes.js
@@ -0,0 +1,8 @@
+var notesArr = [
+    {
+        note: "Here's a note!",
+        id: 0
+    }
+]
+
+module.exports = waitingArray
---
+++ Develop/public/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const notesData = require("../data/notes.js");
+
+
+module.exports = function(app) 
+{
+    app.get("/api/notes", function(req, res) 
+    {
+        res.json(notesData);
+    })
+
+    
+}
